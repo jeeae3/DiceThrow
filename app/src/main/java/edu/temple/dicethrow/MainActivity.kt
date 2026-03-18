fd6cdcbe55e304ab4570bc3d607a5f14784ff4ae
Jeeae <129932495+jeeae3@users.noreply.github.com>
2026-03-18 16:31:27 -0400

    added new fragment container view to layout, added another diefragment so that it is 20 sided and roll at the same time as first diefragment

--- app/src/main/java/edu/temple/dicethrow/MainActivity.kt
+++ app/src/main/java/edu/temple/dicethrow/MainActivity.kt
@@ -21,15 +21,32 @@ class MainActivity : AppCompatActivity() {
                         arguments = Bundle().apply {
                             putInt(DIESIDE, 6)
                         }
-                    })
+                    }
+                )
+                .replace(
+                    R.id.fragmentContainerView2,
+                    DieFragment().apply {
+                        arguments = Bundle().apply {
+                            putInt(DIESIDE, 20)
+                        }
+                    }
+                )
                 .commit()
         }
         val rollButton = findViewById<Button>(R.id.rollDiceButton)
+        rollButton.text = getString(R.string.roll)
+
         rollButton.setOnClickListener {
             val dieFragment = supportFragmentManager.findFragmentById(
                 R.id.fragmentContainerView
             ) as? DieFragment
             dieFragment?.throwDie()
+
+            val dieFragment2 = supportFragmentManager.findFragmentById(
+                R.id.fragmentContainerView2
+            ) as? DieFragment
+            dieFragment2?.throwDie()
+
         }
     }
 }
